36fddcbfa0fcb01090a859daeba9a9ffe41ae3d9
mperdikeas <mperdikeas@gmail.com>
2022-03-16 13:29:53 +0200

    added print

--- tool/src/gr/cognitera/iacs/basic_scheme_adjustment/Right.java
+++ tool/src/gr/cognitera/iacs/basic_scheme_adjustment/Right.java
@@ -1,5 +1,9 @@
 package gr.cognitera.iacs.basic_scheme_adjustment;
 
+import com.google.common.base.MoreObjects;
+import com.google.common.base.MoreObjects.ToStringHelper;
+
+
 import java.math.BigDecimal;
 
 public class Right {
@@ -11,5 +15,22 @@ public class Right {
     public RightType type;
     public BigDecimal unit_value;
 
+    @Override
+    public String toString() {
+        return toStringHelper().toString();
+    }
 
+    protected ToStringHelper toStringHelper() {
+        return MoreObjects.toStringHelper(this)
+            .add("id", id)
+            .add("internal_id", internal_id)
+            .add("source", source)
+            .add("type", type)
+            .add("unit_value", unit_value)
+            ;
+    }
 }
+
+
+
+
